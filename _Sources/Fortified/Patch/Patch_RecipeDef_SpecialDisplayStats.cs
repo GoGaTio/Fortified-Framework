1383d9304ea8dd9b153704db726d37f6e52754a7
AmCh-Q <AmCh-Q@users.noreply.github.com>
2025-08-31 04:21:21 -0400

    Add patch for RecipeDef.SpecialDisplayStats

---
+++ _Sources/Fortified/Patch/Patch_RecipeDef_SpecialDisplayStats.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Fortified;
+
+[HarmonyPatch(typeof(RecipeDef), nameof(RecipeDef.SpecialDisplayStats))]
+internal static class Patch_RecipeDef_SpecialDisplayStats
+{
+    // 参考阅读: https://harmony.pardeike.net/articles/patching-postfix.html#pass-through-postfixes
+    public static IEnumerable<StatDrawEntry> Postfix(IEnumerable<StatDrawEntry> values)
+    {
+        foreach (StatDrawEntry statDrawEntry in values)
+            yield return statDrawEntry;
+
+        // 把额外需要返回的StatDrawEntry直接写到这就完事了
+    }
+}
